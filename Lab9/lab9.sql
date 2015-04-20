babd4843b566f0eb50dec970aeed6e8b6cc63b46
mcp94 <mcpv49@mail.missouri.edu>
2015-04-20 18:00:19 -0500

    Removed excess

--- Lab9/lab9.sql
+++ Lab9/lab9.sql
@@ -1,3 +1,4 @@
+--mcpv49 LAB9
 --Query1
 --Returns the states that intersect a rectangular polygon at the provided coordinates
 SELECT name10 AS name FROM tl_2010_us_state10 WHERE ST_Intersects(coords,ST_SetSRID(ST_MakeBox2D(ST_Point(-110, 35), ST_Point(-109, 36)), 4326));
